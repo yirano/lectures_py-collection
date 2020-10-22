134ced729787a171f8a3e17e3bbc5c6ef0d06336
yirano <yirano@protonmail.ch>
2020-10-22 13:43:27 -0400

    Complete is_empty and add_to_head method for singly_linked_list

--- linked_lists.py
+++ linked_lists.py
@@ -23,6 +23,37 @@ class SLLNode:
         self.next = new_next
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return f'SLL object: {self.head}'
+
+    def is_empty(self):
+        '''Returns True is Linked List is empty // False otherwise'''
+        # if self.head == None:
+        #     return True
+        # else:
+        #     return False
+        return self.head is None
+
+    def add_to_head(self, new_data):
+        '''Add a Node whose data is the new_data argument to the front of the linked list'''
+        temp = SLLNode(new_data)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        pass
+
+    def search(self, data):
+        pass
+
+    def remove(self, data):
+        pass
+
+
 '''
 node = SLLNode('hello')
 node1 = SLLNode('bye')
